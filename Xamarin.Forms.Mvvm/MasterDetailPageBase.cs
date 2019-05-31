91ff7042b115d517e8be15318b71e070492ffe8b
Unknown <jacob.maristany@gmail.com>
2019-05-31 14:34:29 -0500

    initial project

---
+++ Xamarin.Forms.Mvvm/MasterDetailPageBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin.Forms.Mvvm
+{
+    public class MasterDetailPageBase : MasterDetailPage
+    {
+        protected ApplicationBase CurrentApplication => Application.Current as ApplicationBase;
+
+        public virtual async Task Initialize(Dictionary<string, object> navigationParams = null) => await Task.CompletedTask;
+        public virtual async Task<Page> CreateMasterPage() => await Task.FromResult<Page>(new ContentPage());
+        public virtual async Task<Page> CreateDetailPage() => await Task.FromResult<Page>(new ContentPage());
+        public virtual bool UseDetailNavigationPage { get; } = true;
+    }
+}
